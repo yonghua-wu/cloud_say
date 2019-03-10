dd2b06b9a4d109c02b7b4a31e3d7f008a415f9b5
woxiangchixingxing <wi98590@gmail.com>
2019-03-10 21:01:08 +0800

    基本布局

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-cloak class="bg-theme">
+  <div v-cloak>
     <router-view></router-view>
   </div>
 </template>
@@ -9,15 +9,19 @@ export default {
 </script>
 
 <style lang="scss">
-html, body, p, h1, h2, h3, h4, h5, h6 {
+html, body, p, h1, h2, h3, h4, h5, h6, span {
   margin: 0px;
   padding: 0px;
 }
-[v-cloak] {
-  display: none;
+html {
+  font-size: 100%;
 }
 body {
   background-color: #f9f9f9;
+  box-sizing:border-box;
+}
+[v-cloak] {
+  display: none;
 }
 </style>
 
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,9 +1,29 @@
 <template>
   <div>
-    <div class="main-info">
-      <h1>{{ lives.city || '定位中' }}</h1>
-      <h1>{{ lives.temperature || '--' }}℃</h1>
-      <h1>{{ lives.weather || '--' }}</h1>
+    <div class="live">
+      <div class="big-font">{{ lives.city || '定位中' }}</div>
+      <div class="big-font">
+        <span @click="unitConversion" class="">{{ temperature || '--' }}</span>
+        <span @click="unitConversion" class="temperature">{{ defaultUnit==='c' ? '℃' : '℉' }}</span>
+      </div>
+      <div class="big-font">{{ lives.weather || '--' }}</div>
+    </div>
+    <div class="forecast">
+      <div class="day">
+        <div class="week">星期一</div>
+        <div class="temperature">18-25</div>
+        <div class="weather">多云</div>
+      </div>
+      <div class="day">
+        <div class="week">星期一</div>
+        <div class="temperature">18-25</div>
+        <div class="weather">晴</div>
+      </div>
+      <div class="day">
+        <div class="week">星期一</div>
+        <div class="temperature">18-25</div>
+        <div class="weather">晴</div>
+      </div>
     </div>
   </div>
 </template>
@@ -18,7 +38,14 @@ export default {
       // 实时天气
       lives: {},
       // 预报天气
-      forecasts: {}
+      forecasts: {},
+      // 温度单位
+      defaultUnit: 'c',
+      // 华氏温度
+      fahrenheit: {
+        livesTemperature: null,
+        forecastsTemperature: []
+      }
     }
   },
   mounted: function() {
@@ -32,6 +59,7 @@ export default {
       that.$http.get('https://restapi.amap.com/v3/weather/weatherInfo?key='+ config.GAODEKEY +'&extensions=base&city=' + that.adcode).then(res => {
         if(res.data.infocode === '10000') {
           that.lives = res.data.lives[0]
+          that.fahrenheit.livesTemperature = that.CtoF(res.data.lives[0].temperature)
         }
       }).catch(() => {
         console.log('获取实时天气失败')
@@ -40,6 +68,14 @@ export default {
       that.$http.get('https://restapi.amap.com/v3/weather/weatherInfo?key='+ config.GAODEKEY +'&extensions=all&city=' + that.adcode).then(res => {
         if(res.data.infocode === '10000') {
           that.forecasts = res.data.forecasts[0]
+          for(var i=0; i<that.forecasts.casts.length; i++) {
+            var daytemp = that.CtoF(that.forecasts.casts[i].daytemp)
+            var nighttemp = that.CtoF(that.forecasts.casts[i].nighttemp)
+            that.fahrenheit.forecastsTemperature.push({
+              'daytemp': daytemp,
+              'nighttemp': nighttemp
+            })
+          }
         }
       }).catch(() => {
         console.log('获取预报天气失败')
@@ -49,18 +85,92 @@ export default {
     })
   },
   methods: {
-    
+    // 切换温度的单位
+    unitConversion: function () {
+      this.defaultUnit = this.defaultUnit==='c'?'f':'c'
+    },
+    // 摄氏度转华氏度
+    CtoF: function (c) {
+      return parseInt(c * 1.8) + 32
+    },
+    // 将数字星期数转为汉字
+    weekNumToWord: function(week) {
+      switch(week) {
+        case '1':
+        case 1: 
+          return '星期一'
+        case '2':
+        case 2: 
+          return '星期二'
+        case '3':
+        case 3: 
+          return '星期三'
+        case '4':
+        case 4: 
+          return '星期四'
+        case '5':
+        case 5: 
+          return '星期五'
+        case '6':
+        case 6: 
+          return '星期六'
+        case '7':
+        case 7: 
+          return '星期日'
+      }
+    }
+  },
+  computed: {
+    temperature: function() {
+      if (this.defaultUnit === 'f') {
+        return parseInt(this.lives.temperature*1.8)+32
+      } else {
+        return this.lives.temperature
+      }
+    }
   }
 }
 </script>
 <style lang="scss" scoped>
 $marginTop: 16vh;
-.main-info {
+.live {
   margin-top: $marginTop;
   width: 100%;
-  text-align: center;
-  h1 {
-    padding: 1.5vh;
+  margin-bottom: 20vh;
+  .big-font {
+    font-weight: 500;
+    padding: 0.8rem;
+    font-size: 3rem;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: center;
+    .temperature {
+      font-size: 1.5rem;
+    }
+    span {
+      cursor: pointer;
+    }
+  }
+}
+.forecast {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  font-size: 1.4rem;
+  .day {
+    height: 20vh;
+    width: 33%;
+    text-align: center;
+    .week {
+      padding: 0.6rem;
+    }
+    .temperature {
+      padding: 0.6rem;
+    }
+    .weather {
+      padding: 0.6rem;
+    }
   }
 }
 </style>
